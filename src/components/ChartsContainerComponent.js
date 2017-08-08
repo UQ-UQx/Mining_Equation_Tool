abfe6a6191900332ef8082d65b44254f42775994
Ankith Konda <a.konda@uq.edu.au>
2017-08-08 16:26:32 +1000

    Calculate populations using formulas and render charts for pop, tech, and affluence

---
+++ src/components/ChartsContainerComponent.js
@@ -0,0 +1,220 @@
+import React from "react"
+
+import { clearFix } from "polished"
+import {
+    ResponsiveContainer, 
+    ComposedChart, 
+    LineChart, 
+    Line, 
+    BarChart, 
+    Bar, 
+    XAxis, 
+    YAxis, 
+    Tooltip, 
+    Legend, 
+    AreaChart, 
+    Area, 
+    CartesianGrid, 
+    Text
+} from 'recharts';
+import styles from "./_Styles"
+import styled from "styled-components"
+
+
+export default class ChartsContainerComponent extends React.Component{
+
+
+    renderPopulationChart(population_data){
+        let chart = (<PopulationChart>
+                        <ResponsiveContainer>
+                            <LineChart width={600} height={300} data={population_data}
+                                margin={{ top: 10, right: 10, left: 100, bottom: 50 }}>
+
+                            <XAxis 
+                                dataKey="year" 
+                                domain={['auto', 'auto']}  
+                                label={<XAxisLabel text="Year"/>} 
+                                strokeWidth={2} 
+                                tickCount={5}
+                                tick={<AngledTick />}
+                            />
+                        
+                            <YAxis  
+                                dataKey="pop" 
+                                domain={["auto", "auto"]} 
+                                label={<YAxisLabel color={'#41527D'} orientation="left" text="Population"/>}
+                                orientation="left" 
+                                stroke='#41527D' 
+                                strokeWidth={2} 
+                                tickFormatter={CommaFormat} 
+                                yAxisId="left" 
+                            />
+
+                            <Line connectNulls={true} dot={false} yAxisId="left" type="natural" type='linear' dataKey='pop' stroke='#41527D' strokeWidth={3} />
+                            
+                            </LineChart>
+                        </ResponsiveContainer>
+                </PopulationChart>) 
+        return chart
+    }
+
+    renderAffluenceChart(affluence_data){
+        let chart = (<AffluenceChart>
+                        <ResponsiveContainer>
+                            <LineChart width={600} height={300} data={affluence_data}
+                                margin={{ top: 10, right: 10, left: 100, bottom: 50 }}>
+
+                            <XAxis 
+                                dataKey="year" 
+                                domain={['auto', 'auto']}  
+                                label={<XAxisLabel text="Year"/>} 
+                                strokeWidth={2} 
+                                tickCount={5}
+                                tick={<AngledTick />}
+                            />
+                        
+                            <YAxis  
+                                dataKey="affluence" 
+                                domain={["auto", "auto"]} 
+                                label={<YAxisLabel color={'#41527D'} orientation="left" text="Affluence"/>}
+                                orientation="left" 
+                                stroke='#41527D' 
+                                strokeWidth={2} 
+                                tickFormatter={CommaFormat} 
+                                yAxisId="left" 
+                            />
+
+                            <Line connectNulls={true} dot={false} yAxisId="left" type="natural" type='linear' dataKey='affluence' stroke='#41527D' strokeWidth={3} />
+                            
+                            </LineChart>
+                        </ResponsiveContainer>
+                </AffluenceChart>) 
+        return chart
+    }
+
+    renderTechChart(technology_data){
+        let chart = (<TechChart>
+                        <ResponsiveContainer>
+                            <LineChart width={600} height={300} data={technology_data}
+                                margin={{ top: 10, right: 10, left: 100, bottom: 50 }}>
+
+                            <XAxis 
+                                dataKey="year" 
+                                domain={['auto', 'auto']}  
+                                label={<XAxisLabel text="Year"/>} 
+                                strokeWidth={2} 
+                                tickCount={5}
+                                tick={<AngledTick />}
+                            />
+                        
+                            <YAxis  
+                                dataKey="technology" 
+                                domain={["auto", "auto"]} 
+                                label={<YAxisLabel color={'#41527D'} orientation="left" text="Technology"/>}
+                                orientation="left" 
+                                stroke='#41527D' 
+                                strokeWidth={2} 
+                                tickFormatter={CommaFormat} 
+                                yAxisId="left" 
+                            />
+
+                            <Line connectNulls={true} dot={false} yAxisId="left" type="natural" type='linear' dataKey='technology' stroke='#41527D' strokeWidth={3} />
+                            
+                            </LineChart>
+                        </ResponsiveContainer>
+                </TechChart>) 
+        return chart
+    }
+    render(){
+
+        
+        return(<ChartsContainer>
+        
+            {this.renderPopulationChart(this.props.population_data)}
+            {this.renderAffluenceChart(this.props.affluence_data)}
+            {this.renderTechChart(this.props.technology_data)}
+
+            
+        </ChartsContainer>)
+    }
+}
+
+const CommaFormat = (value) => {
+    return value.toLocaleString()
+}
+
+const AngledTick = (props) => {
+    const {x, y, payload} = props
+   	return (
+    	<g transform={`translate(${x},${y})`}>
+        <text x={0} y={0} dy={16} textAnchor="end" fill="#666" transform="rotate(-35)">{payload.value}</text>
+      </g>
+    );
+}
+
+const XAxisLabel = (props) => {
+  const {x, y, width, height} = props.viewBox
+  const cx =  x + (width / 2)
+  const cy =  y + height + 40
+  return (
+    <AxisLabel x={cx} y={cy} textAnchor="middle">
+      {props.text} 
+    </AxisLabel>
+  )
+}
+
+const YAxisLabel = (props) => {
+  const {x, y, width, height} = props.viewBox
+  const { orientation, color } = props
+  const cx = orientation === "right" ?  x + 80 + width : x - 65
+  const cy = (height / 2) + y
+  const rot = `270 ${cx} ${cy}`
+
+  console.log(props)
+  return (
+    <AxisLabel x={cx} y={cy} color={color} transform={`rotate(${rot})`} textAnchor="middle">
+      {props.text}
+    </AxisLabel>
+  )
+}
+
+const chartsHeight = 200
+
+const AxisLabel = styled.text`
+    font-weight:bold;
+    fill:${(props)=> props.color ? props.color : "black" };
+
+`
+
+const Detail = styled.div`
+    color: ${(props)=> props.color ? props.color : "black" };
+    font-weight:bold;
+`
+const ChartsContainer = styled.div`
+    width:100%;
+    height:300px;
+    ${clearFix()}
+`
+
+const PopulationChart = styled.div`
+
+    width:30%;
+    height:${chartsHeight}px;
+    float:left;
+
+`
+
+const AffluenceChart = styled.div`
+
+    width:30%;
+    height:${chartsHeight}px;
+    float:left;
+
+
+`
+const TechChart = styled.div`
+    width:30%;
+    height:${chartsHeight}px; 
+    float:left;
+
+`
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 import axios from "axios"
+import ChartsContainerComponent from "./ChartsContainerComponent"
 import ImpactCharContainer from "./ImpactChartComponent"
 import { clearFix } from "polished"
 import OptionsPanelComponent from "./OptionsPanelComponent"
@@ -18,7 +19,7 @@ export default class Container extends React.Component {
             population_type:"median",
             gdp:75000000000000,
             start_year:2015,
-            end_year:2100,
+            end_year:2070,
             gdp_increase:0.01,
             technology_start:1,
             technology_decrease:0.01
@@ -34,13 +35,27 @@ export default class Container extends React.Component {
     }
 
     getAffluence(gdp_per_capita){
-        return (-1*(gdp_per_capita^2)*0.000001)+(gdp_per_capita*0.0568)
+        return (1*(gdp_per_capita*gdp_per_capita)*0.000001)+(gdp_per_capita*0.0568)
     }
 
-    calculatePopulationValues(type, year){
+    getPopulationForYear(year, type="median"){
+        const multiplier = 1
         switch (type) {
             case "median":
-                return (-462.81*(year^2))+(year*0.0568)
+                console.log("DEFAULT")
+                return (-462.8099673478*(year*year))+(1948623.62597925*year)-2039965212.08985
+                break;
+            case "upper_a":
+                return multiplier*(-240.27082606811*(year*year) + (1056081.92498441*year) - 1145040509.46842)
+                break;
+            case "upper_b":
+                return multiplier*(-329.113947618651*(year*year) + (1413131.02430131*year) - 1503777769.62494)
+                break;
+            case "lower_a":
+                return multiplier*(-596.968721932397*(year*year) + (2481685.02691941*year) - 2569362067.99851)
+                break;
+            case "lower_b":
+                return multiplier*(-565.481589736824*(year*year) + (2358142.21904195*year) - 2448275574.59439)
                 break;
             default:
                 break;
@@ -49,63 +64,68 @@ export default class Container extends React.Component {
 
     render(){
 
-        console.log(population[0][this.state.population_type])
-        let population_data = population.filter((pop_dat)=>{
-            if(pop_dat.year <= this.state.end_year && pop_dat.year >= this.state.start_year){
-                return pop_dat;
-            }
-        })
-
+       
         
 
         let affluence_data = []
         let technology_data = []
+        let population_data = []
         let impact_data = []
         let impact_chart_data = []
 
         let gdp = this.state.gdp
         let tech = this.state.technology_start
 
-        population_data.forEach((pop_dat, pop_ind)=>{
+        for (var year = this.state.start_year; year <= this.state.end_year; year++) {
+           
+            let population = this.getPopulationForYear(year, this.state.population_type)
+            let gdp_per_capita = gdp/population
+            let affluence = this.getAffluence(gdp_per_capita)
+            console.log(affluence, tech, population)
+            let impact = population*affluence*tech
 
-            let gdp_per_capita = gdp/pop_dat[this.state.population_type]
-            console.log(pop_dat[this.state.population_type])
-            let aff = getAffluence(gdp_per_capita)
-            let impact =  aff*tech*pop_dat[this.state.population_type]
-            /*  Affluence Formula y = -1E-06x^2 + 0.0568x
-
-             =(-1*(A27^2)*0.000001)+(A27*0.0568) */
-
-            // console.log(pop_dat.year,pop_dat[this.state.population_type], gdp, aff, tech, aff*tech*pop_dat[this.state.population_type])
+            population_data.push({
+                year:year,
+                pop:population
+            })
             affluence_data.push({
-                year:pop_dat.year,
-                affluence: aff
+                year:year,
+                affluence: affluence
             })
             technology_data.push({
-                year:pop_dat.year,
+                year:year,
                 technology: tech
             })
             impact_data.push({
-                year:pop_dat.year,
+                year:year,
                 impact:impact
             })
             impact_chart_data.push({
-                year:pop_dat.year,
-                pop:pop_dat[this.state.population_type],
+                year:year,
+                pop:population,
                 impact:impact
             })
 
 
-            gdp = this.state.gdp_increase > 0 ? gdp + ( gdp * this.state.gdp_increase) : gdp
-            
-        })
 
+            gdp = this.state.gdp_increase > 0 ? gdp + ( gdp * this.state.gdp_increase) : gdp
+            tech = this.state.technology_decrease > 0 ? tech - ( tech * this.state.technology_decrease) : tech
+        }
+
+    
 
 
         
         return (<ContainerDiv>
 
             <Header>MiningX I=PAT Visualisation</Header>
+
+            <ChartsContainerComponent
+                population_data={population_data}
+                affluence_data={affluence_data}
+                technology_data={technology_data}
+            />
+            
 
              <ImpactCharContainer
                 population_data={population_data}
